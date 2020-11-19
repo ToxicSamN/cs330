1813f1f278a8cfd1beb2a20a9a056e2d8d412750
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2020-11-18 21:39:05 -0700

    Model View Projection Tutorial 4-3 Project Start

---
+++ ModelViewProjection/cmake-build-debug/CMakeFiles/ModelViewProjection.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModelViewProjection.dir/Tutorial4-3.cpp.obj"
+  "ModelViewProjection.exe"
+  "ModelViewProjection.exe.manifest"
+  "ModelViewProjection.pdb"
+  "libModelViewProjection.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ModelViewProjection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
