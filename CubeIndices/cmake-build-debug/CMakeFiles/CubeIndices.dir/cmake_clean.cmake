cda1ba8ea71ff7243c71ce64cba649b9ce70e845
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2020-11-18 22:20:36 -0700

    Building a Cube with Indices Tutorial 4-4 Project Start

---
+++ CubeIndices/cmake-build-debug/CMakeFiles/CubeIndices.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CubeIndices.dir/Tutorial4-4.cpp.obj"
+  "CubeIndices.exe"
+  "CubeIndices.exe.manifest"
+  "CubeIndices.pdb"
+  "libCubeIndices.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CubeIndices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
