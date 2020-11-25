0403ff932cfe6a580628298f60bd3b0e29020e71
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2020-11-25 09:02:56 -0700

    Initializing Practice Activity 6, Assignment 5-1 based on the Turotial 5 code base

---
+++ SammyShuck__wk5_5-1PracticeActivity6/cmake-build-debug/CMakeFiles/SammyShuck__wk5_5_1PracticeActivity6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SammyShuck__wk5_5_1PracticeActivity6.dir/SammyShuck_wk5_PA5-1.cpp.obj"
+  "SammyShuck__wk5_5_1PracticeActivity6.exe"
+  "SammyShuck__wk5_5_1PracticeActivity6.exe.manifest"
+  "SammyShuck__wk5_5_1PracticeActivity6.pdb"
+  "libSammyShuck__wk5_5_1PracticeActivity6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SammyShuck__wk5_5_1PracticeActivity6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
