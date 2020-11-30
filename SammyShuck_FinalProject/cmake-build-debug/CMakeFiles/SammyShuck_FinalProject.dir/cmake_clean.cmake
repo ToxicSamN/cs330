df2af9d9faf3fc42fea53e5810fd6ccb3a5711e6
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2020-11-29 18:27:29 -0700

    Final Project Stub - not tested

---
+++ SammyShuck_FinalProject/cmake-build-debug/CMakeFiles/SammyShuck_FinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SammyShuck_FinalProject.dir/main.cpp.obj"
+  "SammyShuck_FinalProject.exe"
+  "SammyShuck_FinalProject.exe.manifest"
+  "SammyShuck_FinalProject.pdb"
+  "libSammyShuck_FinalProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SammyShuck_FinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
