e00d779ca267ea7fd0e462bacb762fb831d9a891
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2020-12-05 09:19:17 -0700

    Final Project Update

---
+++ SammyShuck_FinalProject/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_bd3aa.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/x3kw/Google Drive/_SNHU_Course_Work/BSCS-Computer_Science/CS-330-J2956_Computer_Graphic_and_Visualization/code/cs330/SammyShuck_FinalProject/cmake-build-debug/CMakeFiles/CMakeTmp/testCXXCompiler.cxx" "/Users/x3kw/Google Drive/_SNHU_Course_Work/BSCS-Computer_Science/CS-330-J2956_Computer_Graphic_and_Visualization/code/cs330/SammyShuck_FinalProject/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_bd3aa.dir/testCXXCompiler.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SammyShuck_FinalProject/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_bd3aa.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_bd3aa.dir/testCXXCompiler.cxx.o"
+  "cmTC_bd3aa"
+  "cmTC_bd3aa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_bd3aa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
