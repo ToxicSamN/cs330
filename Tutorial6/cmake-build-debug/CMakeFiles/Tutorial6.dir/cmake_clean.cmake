97f3ec4ddbe370883d84c01b630e49b9dbed12dc
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2020-12-05 09:19:36 -0700

    Tutorial 6 Initialization

---
+++ Tutorial6/cmake-build-debug/CMakeFiles/Tutorial6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial6.dir/Tutorial6-1.cpp.obj"
+  "Tutorial6.exe"
+  "Tutorial6.exe.manifest"
+  "Tutorial6.pdb"
+  "libTutorial6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
