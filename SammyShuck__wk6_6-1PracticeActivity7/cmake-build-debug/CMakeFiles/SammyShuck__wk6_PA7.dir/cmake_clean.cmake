3058ec269d9580ecddd212f4b0b754b6a0c46b0f
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2020-12-05 19:35:45 -0700

    Practice Activity 7 Assignment 6-1 Complete
    This does NOT run with a CMAKE project. Couldn't figureou out how to get SOIL2 to work with CMAKE projects. Developed this in Eclipse instead.

---
+++ SammyShuck__wk6_6-1PracticeActivity7/cmake-build-debug/CMakeFiles/SammyShuck__wk6_PA7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SammyShuck__wk6_PA7.dir/SammyShuck_wk6_PA7.cpp.obj"
+  "SammyShuck__wk6_PA7.exe"
+  "SammyShuck__wk6_PA7.exe.manifest"
+  "SammyShuck__wk6_PA7.pdb"
+  "libSammyShuck__wk6_PA7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SammyShuck__wk6_PA7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SammyShuck__wk6_6-1PracticeActivity7/cmake-build-debug/CMakeFiles/SammyShuck_wk6_PA7.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Sammy/Google Drive/_SNHU_Course_Work/BSCS-Computer_Science/CS-330-J2956_Computer_Graphic_and_Visualization/code/cs330/SammyShuck__wk6_6-1PracticeActivity7/SammyShuck_wk6_PA7.cpp" "C:/Users/Sammy/Google Drive/_SNHU_Course_Work/BSCS-Computer_Science/CS-330-J2956_Computer_Graphic_and_Visualization/code/cs330/SammyShuck__wk6_6-1PracticeActivity7/cmake-build-debug/CMakeFiles/SammyShuck_wk6_PA7.dir/SammyShuck_wk6_PA7.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib"
+  "C:/TDM-GCC-64/x86_64-w64-mingw32/lib"
+  "C:/TDM-GCC-64/x86_64-w64-mingw32/lib/SOIL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
